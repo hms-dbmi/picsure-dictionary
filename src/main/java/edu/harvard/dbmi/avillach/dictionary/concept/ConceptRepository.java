1bd61c65296dab391fe0a45a23c7b5cf00e51314
Luke Sikina <lucas.skina@gmail.com>
2024-06-24 10:05:54 -0400

    2

--- src/main/java/edu/harvard/dbmi/avillach/dictionary/concept/ConceptRepository.java
+++ src/main/java/edu/harvard/dbmi/avillach/dictionary/concept/ConceptRepository.java
@@ -40,10 +40,12 @@ public class ConceptRepository {
                 concept_node.*,
                 ds.REF as dataset,
                 continuous_min.VALUE as min, continuous_max.VALUE as max,
-                categorical_values.VALUE as values
+                categorical_values.VALUE as values,
+                meta_description.description AS description
             FROM
                 concept_node
                 LEFT JOIN dataset AS ds ON concept_node.dataset_id = ds.dataset_id
+                LEFT JOIN concept_node_meta AS meta_description ON concept_node.concept_node_id = continuous_min.concept_node_id AND continuous_min.KEY = 'description'
                 LEFT JOIN concept_node_meta AS continuous_min ON concept_node.concept_node_id = continuous_min.concept_node_id AND continuous_min.KEY = 'MIN'
                 LEFT JOIN concept_node_meta AS continuous_max ON concept_node.concept_node_id = continuous_max.concept_node_id AND continuous_max.KEY = 'MAX'
                 LEFT JOIN concept_node_meta AS categorical_values ON concept_node.concept_node_id = categorical_values.concept_node_id AND categorical_values.KEY = 'VALUES'
@@ -70,7 +72,7 @@ public class ConceptRepository {
                 ds.REF as dataset,
                 continuous_min.VALUE as min, continuous_max.VALUE as max,
                 categorical_values.VALUE as values,
-                meta_description.description
+                meta_description.description AS description
             FROM
                 concept_node
                 LEFT JOIN dataset AS ds ON concept_node.dataset_id = ds.dataset_id
